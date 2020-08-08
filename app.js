dd8578f33a9556011a5684bd9239aa056b4b5394
Kishan Roy <kishanroy360@gmail.com>
2020-08-08 14:11:02 +0530

    routes updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var bodyParser = require("body-parser"),
+methodOverride = require("method-override"),
 mongoose       = require("mongoose"),
 express        = require("express"),
 app            = express();
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/blog_app', {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -28,7 +30,7 @@ var Blog=mongoose.model("Blog", blogSchema);
 app.get("/",function(req,res){
   res.redirect("/blogs");
 });
-
+//index route
 app.get("/blogs",function(req,res){
     Blog.find({},function(err,blogs){
       if(err){
@@ -40,6 +42,70 @@ app.get("/blogs",function(req,res){
     });
 });
 
+//new route
+app.get("/blogs/new",function(req,res){
+    res.render("new");
+});
+//create route
+app.post("/blogs",function(req,res){
+    //create blog
+    Blog.create(req.body.blog,function(err, newBlog){
+      if(err){
+        res.render("new");
+      }
+      else{
+        res.redirect("/blogs");
+      }
+    });
+});
+
+//show route
+app.get("/blogs/:id", function(req,res){
+    Blog.findById(req.params.id,function(err,foundBlog){
+      if(err){
+        res.redirect("/blogs");
+      }
+      else{
+        res.render("show",{blog: foundBlog});
+      }
+    });
+});
+
+//edit route
+app.get("/blogs/:id/edit",function(req,res){
+    Blog.findById(req.params.id,function(err, foundBlog){
+      if(err){
+        res.redirect("/blogs");
+      }
+      else{
+        res.render("edit",{blog: foundBlog});
+      }
+    });
+});
+//update route
+app.put("/blogs/:id",function(req,res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog,function(err,updatedBlog){
+      if(err){
+        res.redirect("/blogs");
+      }
+      else{
+        res.redirect("/blogs/"+ req.params.id);
+      }
+    });
+});
+//delete route
+app.delete("blogs/:id",function(req,res){
+  //destroy
+  Blog.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+      res.redirect("/blogs");
+    }
+    else{
+      res.redirect("/blogs");
+    }
+  });
+});
+
 app.listen(3000,function(){
     console.log("Server Started!!");
-})
+});
